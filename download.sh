8242e15c8d357aa604fdaa73c6fa0d25e933c4ff
Michael M. Wasser <untoldone@users.noreply.github.com>
2022-11-30 15:36:00 -0500

    dont download nationwide files

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ set -e
 
 export NBM_PROCESS_ID=$(curl https://broadbandmap.fcc.gov/nbm/map/api/published/filing | jq --raw-output ".data[0].process_uuid")
 
-export FILE_ID_LIST=$(curl https://broadbandmap.fcc.gov/nbm/map/api/national_map_process/nbm_get_data_download/$NBM_PROCESS_ID | jq '.data[] | select(.data_type=="Fixed Broadband") | .id')
+export FILE_ID_LIST=$(curl https://broadbandmap.fcc.gov/nbm/map/api/national_map_process/nbm_get_data_download/$NBM_PROCESS_ID | jq '.data[] | select(.data_type=="Fixed Broadband" and .state_fips!=null) | .id')
 
 mkdir -p ./working
 
